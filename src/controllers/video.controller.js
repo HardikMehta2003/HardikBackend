243394357632cb1d5ece5cb3b2e50f4635e0a341
Hardik Mehta <mehtahardik233@gmail.com>
2025-06-30 15:02:48 +0530

    feat: remove unused video duration import from video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,7 +5,6 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadFileCloudinary} from "../utils/cloudinary.js"
-import {getVideoDurationInSeconds} from 'get-video-duration'
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
